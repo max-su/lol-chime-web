1384a8671e82611cce62a396859565e052fa256c
Max Su <max.su7@gmail.com>
2016-08-21 11:03:47 -0500

    added rito games verification for txt route

--- router.js
+++ router.js
@@ -2,4 +2,8 @@ module.exports = function(app) {
     app.get("/", function(req, res) {
         res.sendFile(__dirname + "/views/index.html");
     });
+
+    app.get("/riot.txt", function(req, res) {
+        res.sendFile(__dirname + "/views/riot.txt");
+    });
 };
